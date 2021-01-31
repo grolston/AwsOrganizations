40af622a6fa1b7cc4dbb5fed6894fc75934a536c
George Rolston <grolston@outlook.com>
2021-01-31 09:59:19 -0700

    doc update for SH

--- SecurityHub/audit-account.sh
+++ SecurityHub/audit-account.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+## Note: this is a regional service. Whatever region you run the following command will GuardDuty be
+## configured in. If you want different region add --region to the command
+
 aws securityhub update-organization-configuration --auto-enable
 
-## Note, any already deployed accounts will need to be added via the Admin account under Settings.
+## Note, any already deployed accounts will need to be added/converted via the Admin account under Settings/Accounts.
 ## Select the accounts and the go to actions under the Accounts tab in the Security Hub settings page.
-## In the Actions button, select Add Member.
+## In the Actions button, select Add Member to convert.
--- SecurityHub/management-account.sh
+++ SecurityHub/management-account.sh
@@ -3,5 +3,8 @@
 ## Update var to your Audit/SOC account
 audit_account="123456789"
 
+## Enables Security Hub as an Organizational integrated service
 aws organizations enable-aws-service-access --service-principal securityhub.amazonaws.com
+
+## Sets the admin account for AWS Security Hub for the region you are running the CloudShell in
 aws securityhub enable-organization-admin-account --admin-account-id "${audit_account}"
