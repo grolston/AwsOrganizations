808a682a5beef114920e257270e4a305e9d5779d
George Rolston <grolston@outlook.com>
2021-05-13 08:21:43 -0600

    typo

--- AuditManager/management-account.sh
+++ AuditManager/management-account.sh
@@ -5,8 +5,8 @@ read -p "Enter AWS Account ID to delegate Audit Manager: " ACCOUNT_ID
 
 ## TODO Update CLI
 aws organizations enable-aws-service-access --service-principal auditmanager.amazonaws.com
-register-organization-admin-account --admin-account-id "${ACCOUNT_ID}"
+aws auditmanager register-organization-admin-account --admin-account-id "${ACCOUNT_ID}"
 
-## TODO Deploy GuardDuty in Management Account
+## TODO Deploy Audit Manager in Management Account
 ## The Management account is not controlled by Organization services and needs the service needs to be enabled
-#read -p "Deploy GuardDuty to Management Account? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+#read -p "Configure Audit Manager for Management Account? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
