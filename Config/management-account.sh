014ceeaab3481a22ac44cb71c5d939c641eb4e90
George Rolston <grolston@outlook.com>
2021-05-12 21:28:53 -0600

    config cli added

--- Config/management-account.sh
+++ Config/management-account.sh
@@ -3,6 +3,9 @@
 echo "Configuring AWS Config for your AWS Organization"
 read -p "Enter AWS Account ID to delegate AWS Config: " ACCOUNT_ID
 
-## TODO Update CLI
-aws organizations enable-aws-service-access --service-principal guardduty.amazonaws.com
-aws guardduty enable-organization-admin-account —admin-account-id "${ACCOUNT_ID}"
+
+aws organizations enable-aws-service-access --service-principal config.amazonaws.com
+aws organizations register-delegated-administrator --service-principal config.amazonaws.com --account-id  "${ACCOUNT_ID}"
+
+## TODO Deploy Config stack
+#read -p "Deploy AWS Config to Management Account? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
