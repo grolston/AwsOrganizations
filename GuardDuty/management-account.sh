3909cd1592f6e694962f6aa93779cdf9a1d683da
George Rolston <grolston@outlook.com>
2021-05-12 21:30:56 -0600

    GuardDuty cli

--- GuardDuty/management-account.sh
+++ GuardDuty/management-account.sh
@@ -5,3 +5,6 @@ read -p "Enter AWS Account ID to delegate GuardDuty: " ACCOUNT_ID
 
 aws organizations enable-aws-service-access --service-principal guardduty.amazonaws.com
 aws guardduty enable-organization-admin-account —admin-account-id "${ACCOUNT_ID}"
+
+## TODO Deploy GuardDuty in Management Account
+#read -p "Deploy GuardDuty to Management Account? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
